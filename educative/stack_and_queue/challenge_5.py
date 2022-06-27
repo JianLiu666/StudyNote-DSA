7235ce9255892990f6a54cdc39d71437d446e320
jian_liu <jianliu0616@gmail.com>
2022-06-27 21:19:49 +0800

    update example

--- educative/stack_and_queue/challenge_5.py
+++ educative/stack_and_queue/challenge_5.py
@@ -17,3 +17,74 @@ Sample Output:
     min_stack.min()
     Result = 1
 '''
+
+# Solution: Two Stacks
+# Time Complexity: O(1)
+class Stack:
+    def __init__(self):
+        self.main = []
+        self.length = 0
+
+    def is_empty(self):
+        return self.length == 0
+
+    def size(self):
+        return self.length
+
+    def push(self, data):
+        self.main.append(data)
+        self.length += 1
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        
+        self.length -= 1
+        return self.main.pop()
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        
+        return self.main[-1]
+
+class MinStack:
+    def __init__(self):
+        self.main = Stack()
+        self.mins = Stack()
+
+    def push(self, value):
+        self.main.push(value)
+        if self.mins.is_empty() or self.mins.peek() > value:
+            self.mins.push(value)
+        else:
+            self.mins.push(self.mins.peek())
+
+    def pop(self):
+        self.mins.pop()
+        return self.main.pop()
+
+    def min(self):
+        return self.mins.peek()
+
+if __name__ == "__main__" :
+    stack = MinStack()
+    stack.push(5)
+    stack.push(0)
+    stack.push(2)
+    stack.push(4)
+    stack.push(1)
+    stack.push(3)
+    stack.push(0)
+    print("Main stack:", stack.main.main)
+    print("Min stack:", stack.mins.main)
+    print("Minimum value: " + str(stack.min()))
+    stack.pop()
+    stack.push(-2)
+    print("Main stack:", stack.main.main)
+    print("Min stack:", stack.mins.main)
+    print("Minimum value: " + str(stack.min()))
+    stack.pop()
+    print("Main stack:", stack.main.main)
+    print("Min stack:", stack.mins.main)
+    print("Minimum value: " + str(stack.min()))
