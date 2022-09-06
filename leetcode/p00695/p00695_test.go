e26cda222173744c2f1477e80d47d0abf5f07640
jian_liu <jianliu0616@gmail.com>
2022-09-06 21:01:28 +0800

    add test case

---
+++ leetcode/p00695/p00695_test.go
@@ -0,0 +1,50 @@
+package p00695
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type input struct {
+	grid [][]int
+}
+
+type output struct {
+	ans int
+}
+
+type testdata struct {
+	i input
+	o output
+}
+
+func TestQuestion(t *testing.T) {
+	ast := assert.New(t)
+
+	tds := []testdata{
+		{
+			input{grid: [][]int{
+				{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+				{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+				{0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+			}},
+			output{ans: 6},
+		},
+		{
+			input{grid: [][]int{
+				{0, 0, 0, 0, 0, 0, 0, 0},
+			}},
+			output{ans: 0},
+		},
+	}
+
+	for idx, data := range tds {
+		ast.Equal(data.o.ans, maxAreaOfIsland(data.i.grid), idx+1)
+	}
+}
