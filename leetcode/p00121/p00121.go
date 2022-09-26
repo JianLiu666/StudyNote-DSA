ce6695544892aa17e7de652c34170499a2c7c2d6
jian_liu <jianliu0616@gmail.com>
2022-09-26 22:21:45 +0800

    update golang solution

--- leetcode/p00121/p00121.go
+++ leetcode/p00121/p00121.go
@@ -3,19 +3,24 @@ package p00121
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 func maxProfit(prices []int) int {
-	maximum_profit := 0
-	min_price := prices[0]
-
-	for _, price := range prices {
-		profit := price - min_price
-		if profit > maximum_profit {
-			maximum_profit = profit
-		}
-
-		if price < min_price {
-			min_price = price
-		}
+	minPrice, maxProfit := prices[0], 0
+	for i := 1; i < len(prices); i++ {
+		minPrice = min(minPrice, prices[i])
+		maxProfit = max(maxProfit, prices[i]-minPrice)
 	}
+	return maxProfit
+}
 
-	return maximum_profit
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
