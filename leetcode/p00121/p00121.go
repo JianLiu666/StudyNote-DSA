3a9c362b872ee61342d6cd1389f8573b9212146a
jian_liu <jianliu0616@gmail.com>
2022-10-24 20:18:44 +0800

    review by grind169

--- leetcode/p00121/p00121.go
+++ leetcode/p00121/p00121.go
@@ -3,24 +3,25 @@ package p00121
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 func maxProfit(prices []int) int {
-	minPrice, maxProfit := prices[0], 0
+	min := prices[0] // 紀錄目前為止最低的價格
+	max := prices[0] // 紀錄目前為止最高的價格
+	profit := 0      // 紀錄目前為止最好的獲利
+
 	for i := 1; i < len(prices); i++ {
-		minPrice = min(minPrice, prices[i])
-		maxProfit = max(maxProfit, prices[i]-minPrice)
-	}
-	return maxProfit
-}
+		if prices[i] < min {
+			// 找到更小的買進時機
+			min = prices[i]
+			max = prices[i]
 
-func max(a, b int) int {
-	if a > b {
-		return a
+		} else if prices[i] > max {
+			// 找到更好的賣出時機
+			max = prices[i]
+			// 只有當遇到更好的賣出時機時在比較 profit, 可以減少需要比較的次數
+			if max-min > profit {
+				profit = max - min
+			}
+		}
 	}
-	return b
-}
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+	return profit
 }
