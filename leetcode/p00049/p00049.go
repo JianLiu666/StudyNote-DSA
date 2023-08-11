4b66687879bc7c3b03d71eacf8d673e92f40be14
jian_liu <jianliu0616@gmail.com>
2023-08-11 15:32:04 +0800

    update solution 49

--- leetcode/p00049/p00049.go
+++ leetcode/p00049/p00049.go
@@ -1,44 +1,24 @@
 package p00049
 
-import "strings"
-
 // Time Complexity: O(n^2)
 // Space Complexity: O(n)
 func groupAnagrams(strs []string) [][]string {
-	res := make([][]string, 0)
+	result := [][]string{}
 
-	anagrams_group := map[string]int{}
-	group_index := 0
+	groups := map[[26]int][]string{}
 
-	for _, str := range strs {
-		key := hasher(str)
-		if idx, exists := anagrams_group[key]; exists {
-			res[idx] = append(res[idx], str)
-		} else {
-			res = append(res, []string{str})
-			anagrams_group[key] = group_index
-			group_index++
+	for _, s := range strs {
+		// 透過 array 解決 string immutable 的問題
+		key := [26]int{}
+		for _, r := range s {
+			key[r-'a']++
 		}
+		groups[key] = append(groups[key], s)
 	}
 
-	return res
-}
-
-// Time Complexity: O(n)
-// Space Complexity: O(1)
-func hasher(str string) string {
-	var res strings.Builder
-
-	mapping := [26]int{}
-	for _, ch := range str {
-		mapping[ch-97]++
+	for _, group := range groups {
+		result = append(result, group)
 	}
 
-	for idx, count := range mapping {
-		for i := 0; i < count; i++ {
-			res.WriteByte(byte(idx + 97))
-		}
-	}
-
-	return res.String()
+	return result
 }
