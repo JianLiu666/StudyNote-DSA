fc980b514a926b7f60c6c819ea21b24e655b6143
jian_liu <jianliu0616@gmail.com>
2023-08-16 01:54:20 +0800

    update p150 solution

--- leetcode/p00150/p00150.go
+++ leetcode/p00150/p00150.go
@@ -5,60 +5,29 @@ import "strconv"
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 func evalRPN(tokens []string) int {
-	stack := CreateStack()
-	for _, str := range tokens {
-		if str == "+" || str == "-" || str == "*" || str == "/" {
-			stack.Put(operate(str, stack.Pop(), stack.Pop()))
+	stack := []int{}
+
+	for _, s := range tokens {
+		if s == "+" || s == "-" || s == "*" || s == "/" {
+			last := len(stack) - 1
+			if s == "+" {
+				stack[last-1] += stack[last]
+			}
+			if s == "-" {
+				stack[last-1] -= stack[last]
+			}
+			if s == "*" {
+				stack[last-1] *= stack[last]
+			}
+			if s == "/" {
+				stack[last-1] /= stack[last]
+			}
+			stack = stack[:last]
 		} else {
-			num, _ := strconv.Atoi(str)
-			stack.Put(num)
+			n, _ := strconv.Atoi(s)
+			stack = append(stack, n)
 		}
 	}
 
-	return stack.Pop()
-}
-
-func operate(symbol string, right, left int) int {
-	if symbol == "+" {
-		return left + right
-	} else if symbol == "-" {
-		return left - right
-	} else if symbol == "*" {
-		return left * right
-	}
-
-	return left / right
-}
-
-type Stack struct {
-	arr  []int
-	size int
-}
-
-func CreateStack() Stack {
-	return Stack{
-		arr:  make([]int, 0),
-		size: 0,
-	}
-}
-
-func (s *Stack) IsEmpty() bool {
-	return s.size == 0
-}
-
-func (s *Stack) Put(str int) {
-	s.arr = append(s.arr, str)
-	s.size++
-}
-
-func (s *Stack) Pop() int {
-	if s.IsEmpty() {
-		return -1
-	}
-
-	num := s.arr[s.size-1]
-	s.arr = s.arr[:s.size-1]
-	s.size--
-
-	return num
+	return stack[0]
 }
