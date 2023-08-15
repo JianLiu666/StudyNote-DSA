39414a4baaa5d13760af3fff6f10f5254bf00a5e
jian_liu <jianliu0616@gmail.com>
2023-08-15 16:33:11 +0800

    add new solution for p21

--- leetcode/p00021/p00021.go
+++ leetcode/p00021/p00021.go
@@ -21,3 +21,28 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	list2.Next = mergeTwoLists(list1, list2.Next)
 	return list2
 }
+
+func mergeTwoLists_iteration(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	current := dummy
+
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			current.Next = l1
+			l1 = l1.Next
+		} else {
+			current.Next = l2
+			l2 = l2.Next
+		}
+		current = current.Next
+	}
+
+	if l1 != nil {
+		current.Next = l1
+	}
+	if l2 != nil {
+		current.Next = l2
+	}
+
+	return dummy.Next
+}
