13ca412defab866cf4ca448d98abe97ed87b1133
jian_liu <jianliu0616@gmail.com>
2023-08-19 18:37:50 +0800

    update p98 solution

--- leetcode/p00098/p00098_inorder.go
+++ leetcode/p00098/p00098_inorder.go
@@ -8,59 +8,28 @@ type TreeNode struct {
 
 // Time Complexity: O(n)
 // Space Complexity: O(n)
-func isValidBST_inorder(root *TreeNode) bool {
-	res := []int{}
+func isValidBST(root *TreeNode) bool {
+	monotonicQueue := []int{}
+	return dfs(root, &monotonicQueue)
+}
 
-	s := CreateStrack()
-	for root != nil || !s.Empty() {
-		for root != nil {
-			s.Put(root)
-			root = root.Left
-		}
-
-		root = s.Pop()
-		res = append(res, root.Val)
-		root = root.Right
+func dfs(root *TreeNode, q *[]int) bool {
+	if root == nil {
+		return true
 	}
 
-	for i := 1; i < len(res); i++ {
-		if res[i] <= res[i-1] {
-			return false
-		}
+	if !dfs(root.Left, q) {
+		return false
+	}
+
+	*q = append(*q, root.Val)
+	if len(*q) >= 2 && (*q)[len(*q)-1] <= (*q)[len(*q)-2] {
+		return false
+	}
+
+	if !dfs(root.Right, q) {
+		return false
 	}
 
 	return true
-}
-
-type Stack struct {
-	arr  []*TreeNode
-	size int
-}
-
-func CreateStrack() Stack {
-	return Stack{
-		arr:  make([]*TreeNode, 0),
-		size: 0,
-	}
-}
-
-func (s *Stack) Empty() bool {
-	return s.size == 0
-}
-
-func (s *Stack) Size() int {
-	return s.size
-}
-
-func (s *Stack) Put(val *TreeNode) {
-	s.arr = append(s.arr, val)
-	s.size++
-}
-
-func (s *Stack) Pop() *TreeNode {
-	node := s.arr[s.size-1]
-	s.size--
-	s.arr = s.arr[:s.size]
-
-	return node
 }
