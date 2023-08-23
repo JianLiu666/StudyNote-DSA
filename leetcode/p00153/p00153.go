efd46d2cc9e659c439ca4063675695e7a986613d
jian_liu <jianliu0616@gmail.com>
2023-08-24 01:45:03 +0800

    update p153 solution

--- leetcode/p00153/p00153.go
+++ leetcode/p00153/p00153.go
@@ -3,24 +3,18 @@ package p00153
 // Time Complexity: O(logn)
 // Space Complexity: O(1)
 func findMin(nums []int) int {
-	if len(nums) == 1 {
-		return nums[0]
-	}
-
-	left := 0
-	right := len(nums) - 1
-
-	// it's a sorted array without any rotated
-	if nums[right] > nums[left] {
+	left, right := 0, len(nums)-1
+	if nums[left] < nums[right] {
 		return nums[0]
 	}
 
 	for left != right {
-		mid := (left + right) / 2
-		if nums[mid] > nums[right] {
-			left = mid + 1
-		} else {
+		mid := left + (right-left)/2
+		if nums[mid] < nums[right] {
+			// 屬於遞增區間, 最小值會在最左邊, 也有可能是自己本身
 			right = mid
+		} else {
+			left = mid + 1
 		}
 	}
 
