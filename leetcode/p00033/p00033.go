2d91703597ca961cb8feb07b66c2a7e897969c31
jian_liu <jianliu0616@gmail.com>
2023-08-23 21:51:10 +0800

    update p33 solution

--- leetcode/p00033/p00033.go
+++ leetcode/p00033/p00033.go
@@ -3,29 +3,29 @@ package p00033
 // Time Complexity: O(logn)
 // Space Complexity: O(1)
 func search(nums []int, target int) int {
-	left := 0
-	right := len(nums) - 1
-
-	for left <= right {
-		mid := left + (right-left)/2
+	head, tail := 0, len(nums)-1
+	for head <= tail {
+		mid := (head + tail) / 2
 		if nums[mid] == target {
 			return mid
 		}
 
-		if nums[mid] >= nums[left] {
-			if target > nums[mid] || target < nums[left] {
-				left = mid + 1
+		if nums[mid] < nums[tail] {
+			// 如果右邊有序, 且 target 也在這個範圍內
+			if nums[mid] < target && nums[tail] >= target {
+				head = mid + 1
 			} else {
-				right = mid - 1
+				// 不然就丟到另外一邊
+				tail = mid - 1
 			}
 		} else {
-			if target < nums[mid] || target > nums[right] {
-				right = mid - 1
+			// 左邊有序, 且 target 也在這個範圍
+			if nums[head] <= target && nums[mid] > target {
+				tail = mid - 1
 			} else {
-				left = mid + 1
+				head = mid + 1
 			}
 		}
 	}
-
 	return -1
 }
