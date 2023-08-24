7968028a32c7ac59890d2e967a36e4bb15f6db94
jian_liu <jianliu0616@gmail.com>
2023-08-24 20:09:23 +0800

    update p198 solution

--- leetcode/p00198/p00198.go
+++ leetcode/p00198/p00198.go
@@ -1,26 +1,44 @@
 package p00198
 
 // Time Complexity: O(n)
-// Space Complexity: O(1)
+// Space Complexity: O(n)
 func rob(nums []int) int {
-	if len(nums) < 2 {
-		return nums[0]
+	// 為了後面方便跟 dp 的 index 對齊, 所以補一個用不到的值
+	nums = append([]int{0}, nums...)
+
+	// 每一輪只有可能有兩種狀態: 0:搶, 1:不搶
+	// dp 的長度 len(nums)+1 的原因是, dp[0] 是我們一開始先給定的初始值
+	dp := make([][2]int, len(nums))
+
+	for i := 1; i < len(nums); i++ {
+		// 如果我這輪要搶, 我只有一種選擇: 上輪不搶+這輪搶
+		dp[i][0] = dp[i-1][1] + nums[i]
+
+		// 如果我這輪不搶, 那我就有兩種選擇:
+		// 1. 上輪搶, 2. 上輪不搶
+		// 因此我需要相比較找到上輪搶或不搶時的最大值
+		dp[i][1] = max(dp[i-1][0], dp[i-1][1])
 	}
 
-	previous := nums[0]
-	next := max(nums[1], nums[0])
-
-	for i := 2; i < len(nums); i++ {
-		next, previous = max(nums[i]+previous, next), next
-	}
-
-	return max(next, previous)
+	return max(dp[len(nums)-1][0], dp[len(nums)-1][1])
 }
 
 func max(a, b int) int {
 	if a > b {
 		return a
 	}
-
 	return b
 }
+
+/**
+第 i-1 輪的房子有兩種選擇: 搶 or 不搶
+                          \   /|
+                           \ / |
+                            x  |
+                           / \ |
+                          /   \|
+                         /     |
+第 i 輪的房子有兩種選擇:   搶 or 不搶
+                        v     v -> 不搶就是 +0
+                    這輪搶的話至少會增加 nums[i] 的金額
+*/
